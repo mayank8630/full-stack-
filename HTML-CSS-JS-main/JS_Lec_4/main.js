cceee4fb3fcf8c63e07e9f9cadf96c4385fcc7de
mayank8630 <mayank.singh_cs23@gla.ac.in>
2024-12-05 14:02:02 +0530

    Add files via upload

---
+++ HTML-CSS-JS-main/JS_Lec_4/main.js
@@ -0,0 +1,45 @@
+
+// let person ={
+//   name:"shubham",
+//   age:24,
+//   gender:"male",
+//   height:175
+// }
+
+// console.log(person.name);
+// console.log(person["age"]);
+
+// for(let key in person){
+//   console.log(`${key}->${person[key]}`);
+// }
+
+// let arr = [11,12,131,41,51,16];
+// for(let ind in arr){
+//   console.log(`element at ${ind} - > ${arr[ind]}`);
+// }
+
+// Function
+
+let sum = fun(5,10);
+console.log(sum); 
+let a =10;
+function fun(x,y){
+  console.log("hiii");
+  let sum = x+y; // get error if not passed variable is used
+  return x+y;
+}
+
+// fun2();
+const fun2 = function (){
+  console.log("hii");
+}
+
+fun2();
+// arrow functions 
+
+const arrow = (a,b)=>{
+  console.log("hiiii");
+  return a+b;
+}
+
+arrow(5,9);
---
+++ HTML-CSS-JS-main/JS_Lec_6/main.js
@@ -0,0 +1,6 @@
+console.log("start");
+setTimeout(()=>{
+  console.log("Async JS");
+},2000);
+console.log("end");
+
